ad17d128873aad46e15c42de32b0214e6d143efb
yevhenii-moroziuk <yevhen.moroziuk@hackolade.com>
2025-01-23 12:09:14 +0200

    HCK-9614: remove redundant imports

--- forward_engineering/api.js
+++ forward_engineering/api.js
@@ -1,7 +1,3 @@
-const yaml = require('js-yaml');
-const path = require('path');
-const validationHelper = require('./helpers/validation/helper');
-const { addCommentsSigns, removeCommentLines } = require('./helpers/commentsHelper');
 const { generateModelScript } = require('./helpers/generateModelScript');
 const { validate } = require('./helpers/validation/validate');
 
